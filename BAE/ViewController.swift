7232dbf586c487396011ee3923af97c2ffddb2dd
Kevin Doherty <kevindoherty@Kevins-MacBook-Pro-2.local>
2014-11-20 17:31:53 -0500

    Initial Commit

---
+++ BAE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BAE
+//
+//  Created by Kevin Doherty on 11/20/14.
+//  Copyright (c) 2014 Kevin Doherty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
